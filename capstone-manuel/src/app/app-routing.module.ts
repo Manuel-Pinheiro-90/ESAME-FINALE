94bde68c21217b60524fdf7bf501d8ba0f1b5f05
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-08-30 17:46:40 +0200

    Inizio Auth
    creazione  componenti di Auth

--- capstone-manuel/src/app/app-routing.module.ts
+++ capstone-manuel/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
    { path: 'events', loadChildren: () => import('./pages/events/events.module').then(m => m.EventsModule) },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
   ]
 
 @NgModule({
