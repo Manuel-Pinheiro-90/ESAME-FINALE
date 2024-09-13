055cd1aa01f9fa725f0de5f47a081296925f528e
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-09-13 12:18:52 +0200

    home

--- capstone-manuel/src/app/pages/home/home.component.ts
+++ capstone-manuel/src/app/pages/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor(private el: ElementRef) {}
 
+  ngOnInit(): void {
+    // Puoi inizializzare altre cose se necessario qui
+  }
+
+  ngAfterViewInit(): void {
+    // Seleziona solo gli elementi con la classe .fade-in
+    const fadeInElements = this.el.nativeElement.querySelectorAll('.fade-in');
+
+    const observerOptions = {
+      threshold: 0.5, // L'elemento deve essere visibile almeno per il 50%
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show'); // Aggiunge la classe 'show'
+          observer.unobserve(entry.target); // Smette di osservare una volta visibile
+        }
+      });
+    }, observerOptions);
+
+    // Attiva l'osservatore solo sugli elementi con la classe .fade-in
+    fadeInElements.forEach((element: any) => {
+      observer.observe(element);
+    });
+  }
 }
