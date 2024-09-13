37cf3da4a5a51f56e3bf5a0f43f1df323208f4f6
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-09-13 16:36:17 +0200

    home fix

--- capstone-manuel/src/app/pages/home/home.component.ts
+++ capstone-manuel/src/app/pages/home/home.component.ts
@@ -11,6 +11,18 @@ export class HomeComponent implements OnInit, AfterViewInit {
   eventi: iEvento[] = [];
   currentIndex: number = 0;
   autoSlideInterval: any;
+
+  text1: string =
+    'Crea il tuo personaggio e rendilo indimenticabile! Su LARP HUB hai la possibilità di dare vita a un eroe unico. Ogni dettaglio che aggiungi al tuo personaggio contribuisce a raccontare una storia originale che riflette la tua creatività. Condividi le tue creazioni con la community e lascia che il tuo personaggio ispiri gli altri giocatori nelle loro avventure. Che tu sia un abile guerriero, un saggio mago o un astuto ladro, il mondo di gioco è pronto ad accoglierti e a crescere insieme a te!';
+  text2: string =
+    'Partecipa a numerosi eventi LARP dal vivo, progettati per tutti i gusti e stili di gioco! Su LARP HUB troverai una vasta selezione di eventi organizzati da associazioni di tutta Italia, dalle epiche battaglie fantasy alle intricate trame horror, fino ai racconti di fantascienza e storie post-apocalittiche. Che tu sia un veterano o un novizio, c’è un evento adatto a te!';
+  text3: string =
+    "Partecipa agli Eventi Speciali organizzati da LARP HUB e vivi avventure ancora più emozionanti! Questi eventi esclusivi non solo ti metteranno alla prova con sfide uniche e scenari avvincenti, ma ti daranno anche l'opportunità di vincere fantastici premi. Dai costumi e accessori LARP di alta qualità a equipaggiamenti personalizzati per i tuoi personaggi, ogni evento speciale offrirà ricompense eccezionali per i partecipanti più coraggiosi. Preparati a competere, distinguerti sul campo e ottenere oggetti unici che arricchiranno la tua esperienza LARP. Non perdere l’occasione di lasciare il segno e di portare a casa premi epici!";
+
+  showMore1: boolean = false;
+  showMore2: boolean = false;
+  showMore3: boolean = false;
+
   constructor(private el: ElementRef, private eventService: EventService) {}
 
   ngOnInit(): void {
@@ -49,16 +61,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     // Seleziona solo gli elementi con la classe .fade-in
     const fadeInElements = this.el.nativeElement.querySelectorAll('.fade-in');
-
+    const scrollElements =
+      this.el.nativeElement.querySelectorAll('.scroll-animation');
     const observerOptions = {
-      threshold: 0.5, // L'elemento deve essere visibile almeno per il 50%
+      threshold: 0.4, // L'elemento deve essere visibile almeno per il 50%
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('show'); // Aggiunge la classe 'show'
-          observer.unobserve(entry.target); // Smette di osservare una volta visibile
+          entry.target.classList.add('show');
+          observer.unobserve(entry.target);
         }
       });
     }, observerOptions);
@@ -67,5 +80,18 @@ export class HomeComponent implements OnInit, AfterViewInit {
     fadeInElements.forEach((element: any) => {
       observer.observe(element);
     });
+    scrollElements.forEach((element: any) => {
+      observer.observe(element);
+    });
+  }
+
+  toggleText(section: number): void {
+    if (section === 1) {
+      this.showMore1 = !this.showMore1;
+    } else if (section === 2) {
+      this.showMore2 = !this.showMore2;
+    } else if (section === 3) {
+      this.showMore3 = !this.showMore3;
+    }
   }
 }
