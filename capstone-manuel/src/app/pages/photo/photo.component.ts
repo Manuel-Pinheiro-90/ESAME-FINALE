1bdcf2bb02b8c9e7f4d5b2e96ac8174a1fa72cab
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-09-23 12:10:14 +0200

    immagini

--- capstone-manuel/src/app/pages/photo/photo.component.ts
+++ capstone-manuel/src/app/pages/photo/photo.component.ts
@@ -9,14 +9,34 @@ export class PhotoComponent {
   images = [
     { src: 'assets/img/emp (1).jpg', alt: 'Foto 1' },
     { src: 'assets/img/ork.jpg', alt: 'Foto 2' },
-    { src: 'assets/img/riv3.png', alt: 'Foto 3' },
-    { src: 'assets/img/riv2.png', alt: 'Foto 4' },
-    { src: 'assets/img/emp (1).jpg', alt: 'Foto 5' },
-    { src: 'assets/img/emp (2).jpg', alt: 'Foto 1' },
-    { src: 'assets/img/ork.jpg', alt: 'Foto 2' },
-    { src: 'assets/img/no.png', alt: 'Foto 3' },
-    { src: 'assets/img/riv2.png', alt: 'Foto 4' },
+
     { src: 'assets/img/emp (2).jpg', alt: 'Foto 5' },
+    { src: 'assets/img/foto-sezione-f/1.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/2.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/3.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/4.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/5.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/6.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/7.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/8.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/9.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/10.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/11.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/12.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/13.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/14.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/15.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/16.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/17.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/18.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/19.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/20.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/21.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/22.jpg', alt: 'Foto evento' },
+    { src: 'assets/img/foto-sezione-f/23.jpg', alt: 'Foto evento' },
+
+    { src: 'assets/img/foto-sezione-f/25.jpg', alt: 'Foto evento' },
+
     // Aggiungi altre immagini qui
   ];
 
